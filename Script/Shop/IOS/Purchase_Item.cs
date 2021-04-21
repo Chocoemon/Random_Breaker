82f591bc63aed4a8ebff812ecc30c63304906e07
Nakhyeon Choi <68228162+Chocoemon@users.noreply.github.com>
2021-04-22 01:05:08 +0900

    ios용 상점 파일 추가

---
+++ Script/Shop/IOS/Purchase_Item.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Toast.Gamebase;
+using UnityEngine;
+
+namespace Shop
+{
+    public static class Purchase_Item
+    {
+        public static void RequestNotConsumed()
+        {
+            // 미지급된 아이템 consume flow 진행하기 
+            // 상점 진입시 반드시 실행.
+            // 결제 됐는데, 폰 옮겨타서 설정 다른 경우 포함.
+            //Gamebase.Purchase.RequestItemListOfNotConsumed
+
+        }
+
+        public static void RequestPurchase(string gamebaseProductId, Action<bool> callback)
+        {
+            Gamebase.Purchase.RequestPurchase(gamebaseProductId, (purchasableRecipt, error) =>
+             {
+                 if (Gamebase.IsSuccess(error))
+                 {
+                     SampleLog_Shop.Log("Purchase succeded");
+                     if (gamebaseProductId == "triangle_block")
+                         PlayerPrefs.SetInt("triangle", 1);
+
+                     else if (gamebaseProductId == "rhombus_block")
+                         PlayerPrefs.SetInt("rhombus", 1);
+
+                     else if (gamebaseProductId == "circle_block")
+                         PlayerPrefs.SetInt("circle", 1);
+
+                     callback(true);
+                 }
+
+                 else
+                 {
+                     if (error.code == (int)GamebaseErrorCode.PURCHASE_USER_CANCELED)
+                     {
+                         SampleLog_Shop.Log("user canceled purchase");
+
+                     }
+
+                     else
+                         SampleLog_Shop.Log(string.Format("Purchase failed. error is {0}", error));
+
+                 }
+
+
+             });
+        }
+
+        public static void RequestPurchase_Noads(string gamebaseProductId , Action<bool> callback)
+        {
+            Gamebase.Purchase.RequestPurchase(gamebaseProductId, (purchasableRecipt, error) =>
+            {
+                if (Gamebase.IsSuccess(error))
+                {
+                    SampleLog_Shop.Log("Purchase succeded");
+                    callback(true);
+
+                }
+
+                else
+                {
+                    if (error.code == (int)GamebaseErrorCode.PURCHASE_USER_CANCELED)
+                    {
+                        SampleLog_Shop.Log("user canceled purchase");
+
+                    }
+
+                    else
+                        SampleLog_Shop.Log(string.Format("Purchase failed. error is {0}", error));
+
+                    callback(false);
+                }
+
+
+            });
+        }
+    }
+
+
+
+}
+
---
+++ Script/Shop/IOS/ShopManager.cs
@@ -0,0 +1,265 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace Shop
+{
+    public class ShopManager : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject Triangle, Rect, Rhombous, Star, Equipped, PriceObj, LeftObj, RightObj;
+        [SerializeField]
+        private Button BuyBtn, EquipBtn;
+        private int Item_num = 0;
+        private int IsStar= 0, IsTriangle = 0, IsRhombus = 0;
+
+        private void Start()
+        {
+            int Which_Equipped = PlayerPrefs.GetInt("Block", 0);
+            if (Which_Equipped == 0)
+            {
+                Equipped.SetActive(true);
+                EquipBtn.gameObject.SetActive(false);
+                BuyBtn.gameObject.SetActive(false);
+                PriceObj.gameObject.SetActive(false);
+            }
+
+            else
+            {
+                Equipped.SetActive(false);
+                EquipBtn.gameObject.SetActive(true);
+                BuyBtn.gameObject.SetActive(false);
+                PriceObj.gameObject.SetActive(false);
+            }
+            LeftObj.gameObject.SetActive(false);
+            if (PlayerPrefs.GetInt("star", 0) == 1)
+                IsStar = 1;
+
+            if (PlayerPrefs.GetInt("triangle", 0) == 1)
+                IsTriangle = 1;
+
+            if (PlayerPrefs.GetInt("rhombus", 0) == 1)
+                IsRhombus = 1;
+
+
+        }
+
+        public void Onclick_Purchase()
+        {
+            switch (Item_num) {
+                case 1:
+                    Purchase_Item.RequestPurchase(("Heart_Block"), (callback)=> {
+                        if (callback)
+                        {
+                            Equipped_func();
+                            PlayerPrefs.SetInt("triangle", 1);
+                            PlayerPrefs.SetInt("Block", 1);
+                            IsTriangle = 1;
+                        }
+                    
+                    });
+                    break;
+
+                case 2:
+                    Purchase_Item.RequestPurchase(("star_block"), (callback)=>
+                    {
+                        if (callback)
+                        {
+                            Equipped_func();
+                            PlayerPrefs.SetInt("star", 1);
+                            PlayerPrefs.SetInt("Block", 2);
+                            IsStar = 2;
+                        }
+                    });
+                    break;
+
+                case 3:
+                    Purchase_Item.RequestPurchase(("Rhombus_block"),(callback) =>
+                    {
+                        if (callback)
+                        {
+                            Equipped_func();
+                            PlayerPrefs.SetInt("rhombus", 1);
+                            PlayerPrefs.SetInt("Block", 3);
+                            IsRhombus = 3;
+                        }
+                    });
+                    break;
+            }
+        }
+
+
+        public void Left_Arrow() // 화살표 넘길때 가지고 있는지 유무 판단해서 버튼도 바꾸어 주어야함 
+        {
+            int Equipped_num = PlayerPrefs.GetInt("Block", 0);
+            if (Rect.activeSelf) { 
+                return;
+            }
+
+            if (Triangle.activeSelf)
+            {
+                Triangle.SetActive(false);
+                Rect.SetActive(true);
+                LeftObj.gameObject.SetActive(false);
+                Item_num = 0;
+                if (Item_num == Equipped_num)
+                {
+                    Equipped_func();
+                }
+
+                else
+                    Is_Purchased();
+
+                return;
+            }
+
+            if (Star.activeSelf)
+            {
+                Star.SetActive(false);
+                Triangle.SetActive(true);
+                Item_num = 1;
+                if (Item_num == Equipped_num)
+                {
+                    Equipped_func();
+                }
+
+                else
+                {
+                    if (IsTriangle == 1)
+                        Is_Purchased();
+                    else
+                        Is_not_Purchased();
+                }
+                      
+                return;
+            }
+
+            if (Rhombous.activeSelf)
+            {
+                RightObj.SetActive(true);
+                Rhombous.SetActive(false);
+                Star.SetActive(true);
+                Item_num = 2;
+                if (Item_num == Equipped_num)
+                {
+                    Equipped_func();
+                }
+
+                else
+                {
+                    if(IsStar == 1)
+                        Is_Purchased();
+                    else
+                        Is_not_Purchased();
+                }
+                return;
+            }
+        }
+
+
+        public void Right_Arrow()
+        {
+            int Equipped_num = PlayerPrefs.GetInt("Block", 0);
+            if (Rect.activeSelf)
+            {
+                LeftObj.gameObject.SetActive(true);
+                Triangle.SetActive(true);
+                Rect.SetActive(false);
+                Item_num = 1;
+                if (Item_num == Equipped_num)
+                {
+                    Equipped_func();
+                }
+
+                else
+                {
+                    if(IsTriangle == 1)
+                        Is_Purchased();
+                    else
+                        Is_not_Purchased();
+                }
+                return;
+            }
+
+            if (Triangle.activeSelf)
+            {
+                Triangle.SetActive(false);
+                Star.SetActive(true);
+                Item_num = 2;
+                if (Item_num == Equipped_num)
+                {
+                    Equipped_func();
+                }
+
+                else
+                {
+                    if (IsStar == 1)
+                        Is_Purchased();
+                    else
+                        Is_not_Purchased();
+                }
+                return;
+            }
+
+            if (Star.activeSelf)
+            {
+                Star.SetActive(false);
+                Rhombous.SetActive(true);
+                RightObj.SetActive(false);
+                Item_num = 3;
+                if (Item_num == Equipped_num)
+                {
+                    Equipped_func();
+                }
+
+                else
+                {
+                    if (IsRhombus == 1)
+                        Is_Purchased();
+
+                    else
+                        Is_not_Purchased();
+                }
+                        return;
+            }
+
+            if (Rhombous.activeSelf)
+            {
+                return;
+            }
+        }
+
+
+        public void Onclick_Equip()
+        {
+            PlayerPrefs.SetInt("Block", Item_num);
+            Equipped_func();
+
+        }
+
+        private void Equipped_func()
+        {
+            EquipBtn.gameObject.SetActive(false);
+            Equipped.SetActive(true);
+            BuyBtn.gameObject.SetActive(false);
+            PriceObj.SetActive(false);
+
+        }
+
+        private void Is_Purchased()
+        {
+            EquipBtn.gameObject.SetActive(true);
+            Equipped.SetActive(false);
+            BuyBtn.gameObject.SetActive(false);
+            PriceObj.SetActive(false);
+        }
+
+        private void Is_not_Purchased()
+        {
+            EquipBtn.gameObject.SetActive(false);
+            Equipped.SetActive(false);
+            BuyBtn.gameObject.SetActive(true);
+            PriceObj.SetActive(true);
+        }
+    }
+}
